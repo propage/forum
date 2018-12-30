f143d4ef72999a90bf659733db6c9de46b3151d9
Mike Davies <mike@originet.co.uk>
2018-12-30 21:07:04 +0000

    CRUD for questions

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -7,6 +7,24 @@ use App\User;
 
 class Question extends Model
 {
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'body',
+        'category_id',
+        'user_id',
+    ];
+
+    public function getPathAttribute()
+    {
+        return asset("api/question/$this->slug");
+    }
+
     public function user()
     {
         return $this->belongsTo(User::class);
